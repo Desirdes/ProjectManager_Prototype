ae3db831b1c1ea7c5fe001e539eb8e8e68490963
Desirdes <elexsanford@gmail.com>
2022-04-30 19:02:37 -0400

    Last commit

--- src/app/projectManager/components/editProjectOrRequirementDialog.ts
+++ src/app/projectManager/components/editProjectOrRequirementDialog.ts
@@ -16,26 +16,39 @@ export class EditProjectOrRequirementDialog {
     ) { }
 
     // Public Variables
-    public Title = this.data.isRequirement ? this.data.requirementInfo.Title : '';
-    public Description = this.data.isRequirement ? this.data.requirementInfo.Description : '';
-    public Status = this.data.isRequirement ? this.data.requirementInfo.Status : '';
-    public Priority = this.data.isRequirement ? this.data.requirementInfo.Priority : '';
-    public Members = this.data.isRequirement ? this.data.requirementInfo.Members : [];
+    public Title = this.data.isRequirement ? this.getValue(this.data.requirementInfo.Title) : '';
+    public Description = this.data.isRequirement ? this.getValue(this.data.requirementInfo.Description) : '';
+    public Status = this.data.isRequirement ? this.getValue(this.data.requirementInfo.Status) : '';
+    public Priority = this.data.isRequirement ? this.getValue(this.data.requirementInfo.Priority) : '';
+    public Members = this.data.isRequirement ? this.getValue(this.data.requirementInfo.Members) : [];
 
-    public ProjectTitle = this.data.projectInfo.Title;
-    public ProjectDescription = this.data.projectInfo.Description;
-    public ProjectStartDate = this.data.projectInfo.StartDate;
-    public ProjectEndDate = this.data.projectInfo.EndDate;
-    public ProjectStatus = this.data.projectInfo.Status;
-    public ProjectPriority = this.data.projectInfo.Priority;
-    public ProjectOwner = this.data.projectInfo.Owner;
-    public ProjectMembers = this.data.projectInfo.Members;
-    public ProjectRisks = this.data.projectInfo.Risks;
-    public ProjectGoals = this.data.projectInfo.Goals;
+    public ProjectTitle = this.getValue(this.data.projectInfo.Title);
+    public ProjectDescription = this.getValue(this.data.projectInfo.Description);
+    public ProjectStartDate = this.getValue(this.data.projectInfo.StartDate);
+    public ProjectEndDate = this.getValue(this.data.projectInfo.EndDate);
+    public ProjectStatus = this.getValue(this.data.projectInfo.Status);
+    public ProjectPriority = this.getValue(this.data.projectInfo.Priority);
+    public ProjectOwner = this.getValue(this.data.projectInfo.Owner);
+    public ProjectMembers = this.getValue(this.data.projectInfo.Members);
+    public ProjectRisks = this.getValue(this.data.projectInfo.Risks);
+    public ProjectGoals = this.getValue(this.data.projectInfo.Goals);
 
     public PossibleMembers: MemberInfo[] = this._getPossibleMembers();
 
     // Public Methods
+
+    // This ensures that array data is using the value without actually referenccing the origional data to prevent data from saving even if dialog is cancelled.
+    public getValue(valueToGet){
+      var value;
+
+      if(Array.isArray(valueToGet)){
+        value = Array.from(valueToGet);
+      }else{
+        value = valueToGet;
+      }
+
+      return value;
+    }
 
     public onRemoveItemClick(parentArray, index){
       parentArray.splice(index, 1);
